d9f89e2d35f0d5bd6500129e3a8358c53fe69bf0
HaqikiAchmad <achmadhaqiki9277@gmail.com>
2024-04-05 15:31:54 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama Saya Achmad Haqiki 
+    Saya Bertempat Tinggal Di Daerah Kalideres Jakarta Barat
+    makanan favorit saya semua makanan";
+}
+}
